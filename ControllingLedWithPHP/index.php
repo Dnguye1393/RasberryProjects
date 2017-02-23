b5328a5f9714cbf8bda80aae4b5e18a5c5b0005b
David Nguyen <dnguyen1393@gmail.com>
2017-02-22 21:59:51 -0800

    MISC led files and Duck

--- ControllingLedWithPHP/index.php
+++ ControllingLedWithPHP/index.php
@@ -10,16 +10,13 @@
     <body style="background-color: black;">
     <!-- On/Off button's picture -->
 	<?php
-	$val_array = array(0,0,0,0,0,0,0,0);
+	$val_array = array(0);
 	//this php script generate the first page in function of the file
-	for ( $i= 0; $i<8; $i++) {
 		//set the pin's mode to output and read them
 		system("gpio mode ".$i." out");
-		exec ("gpio read ".$i, $val_array[$i], $return );
-	}
-	//for loop to read the value
-	$i =0;
-	for ($i = 0; $i < 8; $i++) {
+		exec ("gpio read ".$i, $val_array[0], $return );
+
+
 		//if off
 		if ($val_array[$i][0] == 0 ) {
 			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' onclick='change_pin (".$i.");'/>");
@@ -28,7 +25,7 @@
 		if ($val_array[$i][0] == 1 ) {
 			echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".jpg' onclick='change_pin (".$i.");'/>");
 		}
-	}
+
 	?>
 
 	<!-- javascript -->
---
+++ duckWebsite/duck.sh
@@ -0,0 +1,1 @@
+echo url="https://www.duckdns.org/update?domains=dnguyen1393&token=75df0edc-8e83-4cba-bfeb-77a3f6c7318a&ip=" | curl -k -o ~/duckdns/duck.log -K -
